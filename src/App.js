d79fecddeb69bddd94e5b0476948ad680593b556
Priyanshu <sethipriyanshu@outlook.com>
2024-05-15 03:11:42 -0500

    display list using maps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App() {
+  const users = [
+    {name: "priyanshu", age: 19},
+    {name: "Seth", age: 20}
+  ]
   return(
      <div className="App">
-     <h1 className='name'>Priyanshu</h1>
+      {users.map((user, key) => {
+        return <User name = {user.name} age={user.age}/>
+      })}
      </div>
   );
+}
+const User = (props) => {
+  return <div>{props.name}{props.age}</div>
 }
 export default App;
